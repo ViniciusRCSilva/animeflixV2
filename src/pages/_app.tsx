66b97898a4a7c168f62731222bfe7e571aea8eac
Vinicius <viniciusflexa@gmail.com>
2023-02-07 00:20:49 -0300

    added background & topbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import Background from '@/components/Background'
+import Topbar from '@/components/Topbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+      <Background className='flex justify-center'>
+
+        <Topbar />
+
+        <Component {...pageProps} />
+      </Background>
+  ) 
+  
 }
