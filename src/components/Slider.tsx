a28d01519dbc71e5ca6e9d5d9d3e8e292226353b
Vinicius <viniciusflexa@gmail.com>
2023-02-08 21:21:34 -0300

    responsive fixed

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -7,45 +7,39 @@ interface SliderProps{
 }
 
 export default function Slider({ title, children }: SliderProps){
-    const [perViewQuantity, setPerViewQuantity] = useState(5)
-
-    const [windowSize, setWindowSize] = useState({
-        width: 0,
-        height: 0,
-    });
-
     const [ref] = useKeenSlider<HTMLDivElement>({
         loop: true,
         mode: "snap",
         slides: {
-          perView: perViewQuantity,
+          perView: 5,
           spacing: 0,
         },
     })
 
-    function handlePerViewSlider(){
-        if(window.innerWidth < 1024){
-            setPerViewQuantity(1)
-        }else{
-            setPerViewQuantity(5)
-        }
-    }
-
-    useEffect(() => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-
-      window.addEventListener("resize", handlePerViewSlider);
-    }, [])
+    const [refMobile] = useKeenSlider<HTMLDivElement>({
+        loop: true,
+        mode: "snap",
+        slides: {
+          perView: 1,
+          spacing: 0,
+        },
+    })
 
     return(
-        <div className="flex flex-col justify-center px-[4%] pb-[80px]">
-            <p className="text-[1.2em] lg:text-[1.4vw] font-bold text-[#999]">{title}</p> 
-            <div ref={ref} className="flex w-full keen-slider">
-                {children}
+        <>
+            <div className="hidden lg:flex flex-col justify-center px-[4%] pb-[80px]">
+                <p className="text-[1.4vw] font-bold text-[#999]">{title}</p> 
+                <div ref={ref} className="flex w-full keen-slider">
+                    {children}
+                </div>
             </div>
-        </div>
+
+            <div className="flex lg:hidden flex-col justify-center px-[4%] pb-[80px]">
+                <p className="text-[1.2em] font-bold text-[#999]">{title}</p> 
+                <div ref={refMobile} className="flex w-full keen-slider">
+                    {children}
+                </div>
+            </div>
+        </>
     )
 }
