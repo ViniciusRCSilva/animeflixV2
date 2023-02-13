d3427123ca8851b765189458fa5dcaaa3b777ff1
Vinicius <viniciusflexa@gmail.com>
2023-02-13 15:47:02 -0300

    footer page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { EnvelopeSimple, GithubLogo, LinkedinLogo, UserCircle } from "phosphor-react";
+
+export default function Footer(){
+    return(
+        <div className="flex w-full text-gray-unselected my-5 lg:mx-32 px-[4%] text-xs">
+            <div className="flex flex-col max-w-[980px]">
+                <div className="flex mb-[1em] text-white">
+                    <Link href="https://github.com/ViniciusRCSilva" target="_blank">                    
+                        <div className="mr-4 cursor-pointer">
+                            <GithubLogo width={32} height={32} />
+                        </div>
+                    </Link>
+
+                    <Link href="https://www.linkedin.com/in/vinicius-rc-silva/" target="_blank">                    
+                        <div className="mr-4 cursor-pointer">
+                            <LinkedinLogo width={32} height={32} />
+                        </div>
+                    </Link>
+
+                    <Link href="mailto:viniciusflexa@gmail.com" target="_blank">                    
+                        <div className="mr-4 cursor-pointer">
+                            <EnvelopeSimple width={32} height={32} />
+                        </div>
+                    </Link>
+
+                    <Link href="https://vini-portfolio.vercel.app/" target="_blank">                    
+                        <div className="mr-4 cursor-pointer">
+                            <UserCircle width={32} height={32} />
+                        </div>
+                    </Link>
+                </div>
+
+                <div>
+                    <div className="mb-[14px] text-justify lg:text-left">
+                        <p>Este projeto tem apenas objetivo de desafiar as capacidades no desenvolvimento de uma interface semelhante à Netflix.</p>
+                        <p>Gostaria de frisar que não incentivo ou apoio qualquer atividade que viole direitos autorais, incluindo a criação de sites que distribuam filmes ou séries de forma gratuita na internet. Além de ser ilegal, essa prática prejudica a indústria do entretenimento e os artistas envolvidos na produção desses trabalhos. É importante valorizar e respeitar o trabalho desses profissionais, apoiando as formas legais e justas de consumir conteúdo audiovisual.</p>
+                    </div>
+                    <p>© 2023 AnimeFlix, Inc.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,16 @@
-import AnimeContainer from "@/components/AnimeContainer";
 import FeaturedContent from "@/components/FeaturedContent";
-
 import TitleLogo from '../../public/logo-dragon-ball-z.png'
-
-import Slider from "@/components/Slider";
+import AnimeSliders from "@/components/AllAnimeSliders";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full overflow-x-hidden">
       <FeaturedContent logo={TitleLogo} backgroundImage='https://i.pinimg.com/originals/d8/fc/f9/d8fcf96a55264b304b8046f8cfc7b472.jpg' />
 
-      <div className="mt-16 lg:mt-0">
-        <Slider title="Em alta">
-          <AnimeContainer className="keen-slider__slide" image="https://wallpapercave.com/wp/NAA7Ap3.jpg" />
-          <AnimeContainer className="keen-slider__slide" image="https://i.pinimg.com/originals/52/89/a0/5289a0ddf808b686b877e404810903fe.jpg" />
-          <AnimeContainer className="keen-slider__slide" image="https://wallpapercave.com/wp/wp3084736.jpg" />
-          <AnimeContainer className="keen-slider__slide" image="https://beanstalk-9fcd.kxcdn.com/wp-content/uploads/2022/10/Cyberpunk-Edgerunners-Wallpaper-1.jpg" />
-          <AnimeContainer className="keen-slider__slide" image="https://occ-0-3934-586.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUbolrsi2liOMO4EWmHoWAIuzTpefasoKDVN0GSY4WxOoo9OCTGCd_y2ysAfKdaxmLaafS7Upvd0Y4Nw8Hd6I31jS51xbB06Jkxz6OR20J6FRkmja9sIj1WuCE5aLzF5rd0Lyw.jpg?r=be3" />
-          <AnimeContainer className="keen-slider__slide" image="https://coolthemestores.com/wp-content/uploads/2023/01/vinland-saga-wallpaper-background.jpg" />
-        </Slider>
+      <AnimeSliders/>
 
-        <Slider title="Recomendados para você">
-          <AnimeContainer className="keen-slider__slide" image="https://occ-0-3934-586.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUbolrsi2liOMO4EWmHoWAIuzTpefasoKDVN0GSY4WxOoo9OCTGCd_y2ysAfKdaxmLaafS7Upvd0Y4Nw8Hd6I31jS51xbB06Jkxz6OR20J6FRkmja9sIj1WuCE5aLzF5rd0Lyw.jpg?r=be3" />
-          <AnimeContainer className="keen-slider__slide" image="https://wallpapercave.com/wp/wp3084736.jpg" />
-          <AnimeContainer className="keen-slider__slide" image="https://coolthemestores.com/wp-content/uploads/2023/01/vinland-saga-wallpaper-background.jpg" />
-          <AnimeContainer className="keen-slider__slide" image="https://i.pinimg.com/originals/52/89/a0/5289a0ddf808b686b877e404810903fe.jpg" />
-          <AnimeContainer className="keen-slider__slide" image="https://beanstalk-9fcd.kxcdn.com/wp-content/uploads/2022/10/Cyberpunk-Edgerunners-Wallpaper-1.jpg" />
-          <AnimeContainer className="keen-slider__slide" image="https://wallpapercave.com/wp/NAA7Ap3.jpg" />
-        </Slider>
-      </div>
+      <Footer/>
     </div>
   )
 }
