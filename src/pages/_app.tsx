1c71454f92d2d84a6076b0ff19a17da2f63b84df
Vinicius <viniciusflexa@gmail.com>
2023-02-13 14:54:46 -0300

    title and icon update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,23 @@ import Background from '@/components/Background'
 import Topbar from '@/components/Topbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
+    <>
+      <Head>
+        <title>AnimeFlix Brasil</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+
       <Background className='flex justify-center'>
 
         <Topbar />
 
         <Component {...pageProps} />
       </Background>
+    </>
   ) 
   
 }
