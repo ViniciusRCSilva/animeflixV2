7ef205be18b3719009dd5809d814b85ac80429a5
Vinicius <viniciusflexa@gmail.com>
2023-02-13 22:43:26 -0300

    function for the logo link

--- src/pages/series.tsx
+++ src/pages/series.tsx
@@ -3,12 +3,12 @@ import AnimeSliders from "@/components/AllAnimeSliders";
 import Footer from "@/components/Footer";
 
 function logoLink(){
-  const link = "https://i.pinimg.com/originals/60/a9/61/60a96199afa8469b7c3c46810ed86816.png"
+    const link = "https://occ-0-784-2773.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABXo-wdx_H_u8RSf-rQ4qFc19SLHtEuXZGdioZSUfpCxR8ge_ms9JK4fplvKCMF5QQP3Q2n9XJUUw3ery04b9qc_yW6FovPsQWnOHLSf2qE4S.png?r=f91"
 
-  return link
+    return link
 }
 
-export default function Home() {
+export default function Series() {
   return (
     <div className="flex flex-col w-full overflow-x-hidden">
       <FeaturedContent logo={logoLink()} backgroundImage='https://i.pinimg.com/originals/d8/fc/f9/d8fcf96a55264b304b8046f8cfc7b472.jpg' />
