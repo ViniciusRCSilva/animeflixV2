9beb224f2650f37d3c51435ad4b578de6827f38d
Vinicius <viniciusflexa@gmail.com>
2023-02-16 18:37:28 -0300

    randomize animes slider (in progress)

--- src/components/AllAnimeSliders.tsx
+++ src/components/AllAnimeSliders.tsx
@@ -1,45 +1,10 @@
-/* import { useEffect, useState } from "react"; */
-import AnimeContainer from "./AnimeContainer";
-import Slider from "./Slider";
-
-const AnimeImages = [
-  {image: "https://wallpapercave.com/wp/NAA7Ap3.jpg"},
-  {image: "https://i.pinimg.com/originals/52/89/a0/5289a0ddf808b686b877e404810903fe.jpg"},
-  {image: "https://wallpapercave.com/wp/wp3084736.jpg"},
-  {image: "https://beanstalk-9fcd.kxcdn.com/wp-content/uploads/2022/10/Cyberpunk-Edgerunners-Wallpaper-1.jpg"},
-  {image: "https://occ-0-3934-586.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUbolrsi2liOMO4EWmHoWAIuzTpefasoKDVN0GSY4WxOoo9OCTGCd_y2ysAfKdaxmLaafS7Upvd0Y4Nw8Hd6I31jS51xbB06Jkxz6OR20J6FRkmja9sIj1WuCE5aLzF5rd0Lyw.jpg?r=be3"},
-  {image: "https://coolthemestores.com/wp-content/uploads/2023/01/vinland-saga-wallpaper-background.jpg"},
-]
-
-/* type Image = {
-  image: string
-}[] */
+import RandomizeAllAnimeSliders from "./RandomizeAllAnimeSliders";
 
 export default function AnimeSliders(){
-/*   const [randomArray, setRandomArray] = useState<Image>([]);
-    
-  useEffect(() => {
-    const randomizeArray = [...AnimeImages].sort(() => 0.5 - Math.random());
-    setRandomArray(randomizeArray.slice(0, 6));
-  }, []); */
-
   return(
-      <div className="mt-16 lg:mt-0">
-        <Slider title="Em alta">
-          {AnimeImages.map((Anime, i) => {
-            return(
-              <AnimeContainer key={i} className="keen-slider__slide" image={Anime.image} />
-            )
-          })}
-        </Slider>
-
-        <Slider title="Recomendados para você">
-          {AnimeImages.map((Anime, i) => {
-            return(
-              <AnimeContainer key={i} className="keen-slider__slide" image={Anime.image} />
-            )
-          })}
-        </Slider>
+    <div className="mt-16 lg:mt-0">
+      <RandomizeAllAnimeSliders title="Em alta"/>
+      <RandomizeAllAnimeSliders title="Recomendados para você"/>
     </div>
   )
 }
--- src/components/AnimeContainer.tsx
+++ src/components/AnimeContainer.tsx
@@ -5,7 +5,9 @@ interface AnimeProps{
 
 export default function AnimeContainer({ image, className }: AnimeProps){
     return(
-        <div className={`${className}`}>
+        <div 
+            className={`${className}`}
+        >
             <div 
                 style={{backgroundImage: `url(${image})`}} 
                 className="w-[45vw] h-[100px] lg:w-[16vw] lg:h-[20vh] bg-cover bg-center lg:rounded-md delay-300 lg:hover:scale-125 transition-all cursor-pointer" 
